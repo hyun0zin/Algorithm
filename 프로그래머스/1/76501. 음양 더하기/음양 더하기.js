1ff55f282ae6a4a1af06a372d27ed4d367ee76b4
hyun0zin <154870548+hyun0zin@users.noreply.github.com>
2024-01-03 12:41:26 +0900

    [level 1] Title: 음양 더하기, Time: 0.17 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.js
@@ -0,0 +1,19 @@
+function solution(absolutes, signs) {
+    // 1. return 해야 하는 answer 생성
+    var answer = 0;
+
+    // 2. 두 배열을 비교해가며 값을 더하거나 빼줄 것.
+    // 2-1. 두 배열은 크기가 같음
+    for (var i = 0; i < absolutes.length; i++) {
+        // 부호 (+,-) 에 따른 처리
+        if (signs[i] === true) {
+            // 값이 true인 경우(양수)
+            answer = answer + absolutes[i];
+        } else if (signs[i] === false) {
+            // 값이 false인 경우(음수)
+            answer = answer + - absolutes[i];
+        }
+    }
+    // 3. 출력
+    return answer;
+}
