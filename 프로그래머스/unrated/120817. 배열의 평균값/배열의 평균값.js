87cf7ce96e424cd6b6c1f4b5a979a0ed5e022eac
hyun0zin <154870548+hyun0zin@users.noreply.github.com>
2024-01-04 12:51:06 +0900

    [level unrated] Title: 배열의 평균값, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/120817. 배열의 평균값/배열의 평균값.js
+++ 프로그래머스/unrated/120817. 배열의 평균값/배열의 평균값.js
@@ -1,7 +1,15 @@
+// function solution(numbers) {
+//     let array = numbers;
+//     const result = array.reduce(function add(a,b){
+//         return a+b;
+//     })
+//     return result/array.length;
+// }
+
 function solution(numbers) {
-    let array = numbers;
-    const result = array.reduce(function add(a,b){
-        return a+b;
-    })
-    return result/array.length;
+    var sum = 0;
+    for (i=0; i<numbers.length; i++){
+       sum = sum + numbers[i];
+    }
+    return sum / numbers.length;
 }
