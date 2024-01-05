3c07e16c760bd0a39f95259f202c9b25c6709e79
hyun0zin <154870548+hyun0zin@users.noreply.github.com>
2024-01-05 10:57:55 +0900

    [level 1] Title: 짝수와 홀수, Time: 0.02 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12937. 짝수와 홀수/짝수와 홀수.js
+++ 프로그래머스/1/12937. 짝수와 홀수/짝수와 홀수.js
@@ -1,9 +1,15 @@
-function solution(num) {
+// 조건문 이용해서 풀기
+// function solution(num) {
   
-    if(num % 2 === 0){
-       return "Even"; 
-    } else {
-        return "Odd";
-    };
+//     if(num % 2 === 0){
+//        return "Even"; 
+//     } else {
+//         return "Odd";
+//     };
  
-};
+// };
+
+// 삼항 연산자 사용하기
+function solution(num){
+    return (num%2===0) ? "Even" : "Odd";
+}
