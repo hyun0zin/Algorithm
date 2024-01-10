97ade2f7f33585e8f74915cf9e9351171da1a577
hyun0zin <154870548+hyun0zin@users.noreply.github.com>
2024-01-10 09:55:26 +0900

    [level 1] Title: 자연수 뒤집어 배열로 만들기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.js
+++ 프로그래머스/1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.js
@@ -3,12 +3,14 @@ function solution(n) {
     const arr = String(n).split('');
    
     for(let i=arr.length-1; i>=0; i--) {
-       answer += arr[i] 
+       // answer += arr[i] 
+        answer.push(Number(arr[i]));
     }
     
-    const newArr = Array.from(answer, function(str){
-        return Number(str)
-    });
+    // const newArr = Array.from(answer, function(str){
+    //     return Number(str)
+    // });
     
-    return newArr;
+    
+    return answer;
 }
