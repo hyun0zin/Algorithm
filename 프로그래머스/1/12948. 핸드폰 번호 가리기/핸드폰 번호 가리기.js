de8a8526a1a437556132ff8b33bda77eab5bb879
Hyunjin KIM <154870548+hyun0zin@users.noreply.github.com>
2024-01-25 10:38:55 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.11 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
@@ -1,7 +1,10 @@
 function solution(phone_number) {
-    const changeNumber= phone_number.slice(0,phone_number.length-4);
-    const starCount = "*".repeat(changeNumber.length);
-    const answer = phone_number.replace(changeNumber, starCount)
+    // const changeNumber= phone_number.slice(0,phone_number.length-4);
+    // const starCount = "*".repeat(changeNumber.length);
+    // const answer = phone_number.replace(changeNumber, starCount)
+
+    // 정규식 표현     
+    const answer = phone_number.replace(/\d(?=\d{4})/g, "*")
    
  return answer;
     
