0dd12993c30bc9348863993580159c6693f5d334
Hyunjin KIM <154870548+hyun0zin@users.noreply.github.com>
2024-01-31 09:58:56 +0900

    [level 1] Title: 문자열 내림차순으로 배치하기, Time: 2.02 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.js
+++ 프로그래머스/1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.js
@@ -1,25 +1,9 @@
-// function solution(s) {
-//     // s : "Zabed" -> [Z, a,b,c,d] : map
-//     const newArr = s.split('').map(String)
-//     // console.log(newArr)
-    
-//     // 삽입 정렬
-//     for (let i=1; i<newArr.length; i++){
-//         for(let j=i; j<0; j--){
-//             if(newArr[j-1] < newArr[j]){
-//                 let temp = newArr[j-1];
-//                 newArr[j-1] = newArr[j];
-//                 newArr[j] = temp;
-                
-//             }else{
-//                 break;
-//             }
-            
-//         }
-//     }
-//     return newArr.join('')
-// }
-
 function solution(s) {
-    return s.split('').sort().reverse().join('');
+    const newArr = s.split('')
+    let answer = newArr.sort().reverse().join('')
+    console.log(answer)
+    return answer
 }
+// function solution(s) {
+//     return s.split('').sort().reverse().join('');
+// }
