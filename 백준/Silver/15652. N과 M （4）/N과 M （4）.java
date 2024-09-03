87d01f26ed460aaecbd715c379b99f0f64751da7
Hyunjin KIM <154870548+hyun0zin@users.noreply.github.com>
2024-09-03 12:14:46 +0900

    [Silver III] Title: N과 M (4), Time: 204 ms, Memory: 19296 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Main {
+	static StringBuilder sb = new StringBuilder();
+	static int N, M;
+	static int[] selected;
+	static boolean[] visited;
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		N = sc.nextInt();
+		M = sc.nextInt();
+		
+		visited = new boolean[N]; // 방문 여부 체크
+		selected = new int[M]; // 경우의 수 배열 
+		
+		select(0, 0);
+		System.out.println(sb);
+	}
+	
+	static void select(int idx, int count) {
+		if(count == M) { // M개 고르면 출력 
+			for(int i = 0; i < M; i++) {
+				sb.append(selected[i]).append(" ");
+			}
+			sb.append("\n");
+			return;
+		}
+		
+		for(int i = idx; i < N; i++) { // N까지 
+			selected[count] = i+1; 
+			select(i, count+1);
+			
+		}
+	}
+	
+}
