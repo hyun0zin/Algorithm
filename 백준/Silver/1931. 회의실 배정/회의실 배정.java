416e7cb60a960a52b27e83bfc159a4a08d243b1e
Hyunjin KIM <154870548+hyun0zin@users.noreply.github.com>
2024-10-23 14:38:29 +0900

    [Silver I] Title: 회의실 배정, Time: 928 ms, Memory: 165864 KB -BaekjoonHub

---
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.java
@@ -0,0 +1,65 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.PriorityQueue;
+import java.util.Scanner;
+
+// BOJ1931_회의실배정
+public class Main {
+
+	public static class Node implements Comparable<Node> {
+		int start, end;
+
+		public Node(int start, int end) {
+			super();
+			this.start = start;
+			this.end = end;
+		}
+
+		@Override
+		public int compareTo(Node o) {
+			// 끝나는 시간이 같다면, 시작 시간 기준으로 정렬하기
+			if (this.end == o.end) {
+				return this.start - o.start;
+			}
+			// 회의가 끝나는 시간을 기준으로 정렬하기 => 끝나는 시간에 이은 다음 회의 배치에 유리
+			return this.end - o.end;
+		}
+
+		@Override
+		public String toString() {
+			return "Time [start=" + start + ", end=" + end + "]";
+		}
+	}
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int N = sc.nextInt(); // 회의의 수
+
+		ArrayList<Node> list = new ArrayList<>();
+		for (int i = 0; i < N; i++) {
+			int startTime = sc.nextInt();
+			int endTime = sc.nextInt();
+			list.add(new Node(startTime, endTime));
+		}
+
+		Collections.sort(list);
+//		System.out.println(list.toString());
+
+		int ans = 0; // 정답 수
+		int endTime = 0; // 끝나는 수 갱신을 위한 변수
+
+		// list 내의 모든 노드를 돌면서, 
+		for (Node meeting : list) {
+			// 다음 회의 시작시간이 이전 회의 끝나는 시간보다 크면,
+			// 끝나는 시간 갱신
+			if (meeting.start >= endTime) {
+				endTime = meeting.end;
+				ans++; // 회의 수 up
+			}
+		}
+		
+		System.out.println(ans);
+
+	}
+}
