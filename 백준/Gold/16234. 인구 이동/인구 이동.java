db377bd8f4db766e9b76c0fb1ea3d4727c9ef1a1
Hyunjin KIM <154870548+hyun0zin@users.noreply.github.com>
2024-12-11 09:54:34 +0900

    [Gold IV] Title: 인구 이동, Time: 524 ms, Memory: 296204 KB -BaekjoonHub

--- 백준/Gold/16234. 인구 이동/인구 이동.java
+++ 백준/Gold/16234. 인구 이동/인구 이동.java
@@ -8,8 +8,8 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 /* BOJ16234_인구이동
-시간 : 
-메모리 : 
+시간 : 544 ms
+메모리 : 294672 KB
 *
 */
 
@@ -19,25 +19,14 @@ public class Main {
 	static boolean[][] visited;
 	static int[] dr = { -1, 1, 0, 0 };
 	static int[] dc = { 0, 0, -1, 1 };
-	static List<Node> list;
-
-	public static class Node {
-		int x;
-		int y;
-
-		public Node(int x, int y) {
-			this.x = x;
-			this.y = y;
-		}
-	}
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 
-		 N = Integer.parseInt(st.nextToken());
-		 L = Integer.parseInt(st.nextToken());
-		 R = Integer.parseInt(st.nextToken());
+		N = Integer.parseInt(st.nextToken());
+		L = Integer.parseInt(st.nextToken());
+		R = Integer.parseInt(st.nextToken());
 
 		board = new int[N][N];
 
@@ -58,17 +47,8 @@ public class Main {
 			for (int i = 0; i < N; i++) {
 				for (int j = 0; j < N; j++) {
 					if (!visited[i][j]) {
-
-						int totalSum = bfs(i, j); // 인구 이동할 총 인원
-						int NodeNum = list.size(); // 인구 이동 할 수 있는 모든 노드 수
-						int avg = totalSum / NodeNum; // 인구 이동 후, 각 노드별 인구 수
-
-						// list 내의 모든 Node를 돌면서, 새로운 인구로 바꾸기
-						if (list.size() > 1) {
-							for (Node n : list) {
-								board[n.x][n.y] = avg;
-							}
-							isMove = true; // 인구 이동 계속 진행함
+						if (bfs(i, j)) {
+							isMove = true;
 						}
 					}
 				}
@@ -86,36 +66,48 @@ public class Main {
 
 	}
 
-	static int bfs(int r, int c) {
-		Queue<Node> queue = new LinkedList<>();
-		list = new ArrayList<>();
+	static boolean bfs(int r, int c) {
+		Queue<int[]> queue = new LinkedList<>();
+		List<int[]> list = new ArrayList<>();
 
-		queue.add(new Node(r, c)); // bfs 돌면서 이동 가능한 Node queue에 넣기
-		list.add(new Node(r, c)); // 인구 이동할 Node 몇 개인지 -> list에 추가
+		queue.add(new int[] { r, c }); // bfs 돌면서 이동 가능한 Node queue에 넣기
+		list.add(new int[] { r, c }); // 인구 이동할 Node 몇 개인지 -> list에 추가
 		visited[r][c] = true;
 		int sum = board[r][c];
 
 		while (!queue.isEmpty()) {
-			Node curr = queue.poll();
+			int[] curr = queue.poll();
+			int currR = curr[0];
+			int currC = curr[1];
 
 			for (int dir = 0; dir < 4; dir++) {
-				int nr = curr.x + dr[dir];
-				int nc = curr.y + dc[dir];
+				int nr = currR + dr[dir];
+				int nc = currC + dc[dir];
 
 				if (nr < 0 || nr >= N || nc < 0 || nc >= N || visited[nr][nc])
 					continue;
 
-				int diff = Math.abs(board[nr][nc] - board[curr.x][curr.y]);
+				int diff = Math.abs(board[nr][nc] - board[currR][currC]);
 				if (L <= diff && diff <= R) {
-					queue.add(new Node(nr, nc));
-					list.add(new Node(nr, nc));
+					queue.add(new int[] { nr, nc });
+					list.add(new int[] { nr, nc });
 					sum += board[nr][nc];
 					visited[nr][nc] = true;
 				}
 			}
 
 		}
-		return sum; // 인구이동 할 수 있는 모든 Node의 사람 수
+
+		int avg = sum / list.size(); // 인구 이동 후, 각 노드별 인구 수
+
+		// list 내의 모든 Node를 돌면서, 새로운 인구로 바꾸기
+		if (list.size() > 1) {
+			for (int[] arr : list) {
+				board[arr[0]][arr[1]] = avg;
+			}
+			return true; // 인구 이동이 발생
+		}
+		return false; // 인구 이동이 발생하지 않음 
 	}
 
 }
