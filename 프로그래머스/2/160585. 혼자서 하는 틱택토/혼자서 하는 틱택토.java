0dc189198190494c29db10cb7d6c6bce1b9a2d1b
Hyunjin KIM <154870548+hyun0zin@users.noreply.github.com>
2025-01-19 21:10:21 +0900

    [level 2] Title: 혼자서 하는 틱택토, Time: 0.10 ms, Memory: 77.1 MB -BaekjoonHub

--- 프로그래머스/2/160585. 혼자서 하는 틱택토/혼자서 하는 틱택토.java
+++ 프로그래머스/2/160585. 혼자서 하는 틱택토/혼자서 하는 틱택토.java
@@ -18,12 +18,12 @@ class Solution {
             }
         }
         
-        for(int i=0; i<3; i++){
-            for(int j=0; j<3; j++){
-                System.out.print(map[i][j]);
-            }
-            System.out.println();
-        }
+        // for(int i=0; i<3; i++){
+        //     for(int j=0; j<3; j++){
+        //         System.out.print(map[i][j]);
+        //     }
+        //     System.out.println();
+        // }
 
         
         int answer = 1;
@@ -51,11 +51,13 @@ class Solution {
     }
     
     public static boolean check(char c){
+        // 가로 또는 세로 빙고
         for(int i = 0; i < 3; i++){
             if(map[i][0] == c && map[i][1] == c && map[i][2] == c) return true;
             if(map[0][i] == c && map[1][i] == c && map[2][i] == c) return true;
         }
         
+        // 대각선 빙고
         if(map[0][0] == c && map[1][1] == c && map[2][2] == c) return true;
         if(map[0][2] == c && map[1][1] == c && map[2][0] == c) return true;
         
