503baf89f4ecd868907c127bbf045d7e1f30906c
Hyunjin KIM <koujie11@gmail.com>
2025-04-20 19:09:04 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 360.38 ms, Memory: 117 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/README.md
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/README.md
@@ -1,10 +1,10 @@
 # [level 2] 연속 부분 수열 합의 개수 - 131701 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131701?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131701) 
 
 ### 성능 요약
 
-메모리: 127 MB, 시간: 262.53 ms
+메모리: 117 MB, 시간: 360.38 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 04월 19일 00:37:32
+2025년 04월 20일 19:09:03
 
 ### 문제 설명
 
--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
@@ -1,29 +1,33 @@
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 class Solution {
     public int solution(int[] elements) {
+        int answer = 0;
         int n = elements.length;
-        int[] doubled = new int[n * 2];
+        int[] doubleArr = new int[n * 2];
 
-        // 원형 수열 구현을 위해 배열 2배로 복사
-        for (int i = 0; i < n * 2; i++) {
-            doubled[i] = elements[i % n];
+        // 원형 수열 구현을 위해 배열 크기를 2배로 만들기
+        // [7,9,1,1,4,7,9,1,1,4]
+        for(int i= 0; i < n; i++){
+            doubleArr[i] = elements[i];
+            doubleArr[i + n] = elements[i];
         }
 
         Set<Integer> sumSet = new HashSet<>();
 
         // 길이 1부터 n까지의 연속 부분 수열 합 계산
-        for (int len = 1; len <= n; len++) {
-            for (int start = 0; start < n; start++) {
+        for(int len=1; len<=n; len++){
+            for(int i = 0; i<n; i++){
                 int sum = 0;
-                for (int i = start; i < start + len; i++) {
-                    sum += doubled[i];
+                for(int j=i; j<i+len; j++){
+                    sum += doubleArr[j];
                 }
                 sumSet.add(sum);
             }
         }
+        
+        answer = sumSet.size();
 
-        return sumSet.size();
+        return answer;
     }
 }
