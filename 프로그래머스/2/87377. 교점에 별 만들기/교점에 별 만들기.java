fdacd334fd9d8ee42029a211959617fb3edde139
Hyunjin KIM <koujie11@gmail.com>
2025-05-07 23:47:48 +0900

    [level 2] Title: 교점에 별 만들기, Time: 27.65 ms, Memory: 73.8 MB -BaekjoonHub

--- 프로그래머스/2/87377. 교점에 별 만들기/교점에 별 만들기.java
+++ 프로그래머스/2/87377. 교점에 별 만들기/교점에 별 만들기.java
@@ -11,18 +11,18 @@ class Solution {
             this.y = y;
         }
         
-        @Override
-        public boolean equals(Object obj) {
-            if (this == obj) return true;
-            if (!(obj instanceof Point)) return false;
-            Point other = (Point) obj;
-            return this.x == other.x && this.y == other.y;
-        }
+//         @Override
+//         public boolean equals(Object obj) {
+//             if (this == obj) return true;
+//             if (!(obj instanceof Point)) return false;
+//             Point other = (Point) obj;
+//             return this.x == other.x && this.y == other.y;
+//         }
 
-        @Override
-        public int hashCode() {
-            return Objects.hash(x, y);
-        }
+//         @Override
+//         public int hashCode() {
+//             return Objects.hash(x, y);
+//         }
     }
     
     
